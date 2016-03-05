bfa3ffb95ed9e0260aaa56de12fa405f49d80d9c
Marius Mandal <mariusmandal@gmail.com>
2016-03-05 12:46:30 +0100

    sjekk filrettigheter

--- _playbackserver/api/status.php
+++ _playbackserver/api/status.php
@@ -4,4 +4,8 @@ $info = new stdClass;
 $info->diskspace = diskfreespace(__DIR__);
 $info->total_diskspace = disk_total_space(__DIR__);
 
+// Writeable upload directory
+$info->writeable_upload_folder = is_writeable( dirname(__DIR__). '/upload/upload_temp/');
+$info->writeable_data_folder = is_writeable( dirname(__DIR__). '/upload/data/');
+
 die(json_encode($info));
