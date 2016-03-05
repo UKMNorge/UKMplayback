74b18c6b13eda82ac905aa5436d69807490c05e1
Marius Mandal <mariusmandal@gmail.com>
2016-03-05 11:40:49 +0100

    Status-api

---
+++ _playbackserver/api/status.php
@@ -0,0 +1,8 @@
+<?php
+
+$info = new stdClass;
+$info->queue = new stdClass;
+$info->diskspace = diskfreespace(__DIR__);
+$info->total_diskspace = disk_total_space(__DIR__);
+
+die(json_encode($info));
