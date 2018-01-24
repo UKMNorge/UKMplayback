cde96ea045e579a768552e7ecbc21c24653248a0
Marius Mandal <mariusmandal@gmail.com>
2018-01-24 11:58:39 +0100

    Bruk https

--- _playbackserver/upload/jQupload_cors.php
+++ _playbackserver/upload/jQupload_cors.php
@@ -2,7 +2,7 @@
 ################################################
 ## SET ALL HEADERS AND ACTUALLY PERFORM UPLOAD 
 header('Access-Control-Allow-Headers: true');
-header('Access-Control-Allow-Origin: http://ukm.no');
+header('Access-Control-Allow-Origin: https://ukm.no');
 header('Access-Control-Request-Method: OPTIONS, HEAD, GET, POST, PUT, PATCH, DELETE');
 header('Access-Control-Allow-Credentials: true');
 
