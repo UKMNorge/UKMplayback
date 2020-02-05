ea0969085a727c0a32b01a097298aa358c42b8e4
Marius Mandal <mariusmandal@gmail.com>
2020-02-05 13:32:41 +0100

    formatering

--- UKMplayback.js
+++ UKMplayback.js
@@ -1,49 +1,52 @@
-jQuery(document).ready(function(){
+jQuery(document).ready(function() {
     jQuery('#fileupload_playback').fileupload({
         // Uncomment the following to send cross-domain cookies:
-        xhrFields: {withCredentials: true},
-        url: 'https://playback.'+ UKM_HOSTNAME +'/upload/jQupload_recieve.php',
+        xhrFields: { withCredentials: true },
+        url: 'https://playback.' + UKM_HOSTNAME + '/upload/jQupload_recieve.php',
         fileTypes: '^audio\/(.)+',
         autoUpload: true,
-        formData: {'season': jQuery('#season').val(),
-	        	   'pl_id': jQuery('#pl_id').val()
-	        	   },
-        progressall: function (e, data) {
-	        var progress = parseInt(data.loaded / data.total * 100, 10);
-	        jQuery('#uploadprogress').attr('value', progress);
-	    },
-    }).bind('fileuploaddone', function(e, data){
-		    if(!data.result.success) {
-			    fileUploadError( data.result );
-		    } else {
-			   	jQuery('#uploading').slideUp();
-			   	jQuery('#uploaded').slideDown();
-			    jQuery('#filename').val(data.result.files[0].file_id);
-			    jQuery('#submitbutton').attr('disabled','').removeAttr('disabled');
-			}
-	}).bind('fileuploadstart', function(){
-		jQuery('#filechooser').slideUp();
-		jQuery('#uploading').slideDown();
-		jQuery('#fileupload_dropzone').fadeOut();
-	});
-	
-   if(jQuery('#fileupload_playback').html() !== 'undefined' && jQuery('#fileupload_playback').html() !== undefined) {
-	    if (jQuery.support.cors) {
-	        jQuery.ajax({
-	            url: 'https://playback.' + UKM_HOSTNAME + '/upload/jQupload_cors.php',
-	            type: 'HEAD'
-	        }).fail(function () {
-	        	var result = {'success': false,
-		        			  'message': 'Beklager, playbackserveren er ikke tilgjengelig akkurat nå'};
-	            fileUploadError( result );
-	        });
-	    }
-	}
+        formData: {
+            'season': jQuery('#season').val(),
+            'pl_id': jQuery('#pl_id').val()
+        },
+        progressall: function(e, data) {
+            var progress = parseInt(data.loaded / data.total * 100, 10);
+            jQuery('#uploadprogress').attr('value', progress);
+        },
+    }).bind('fileuploaddone', function(e, data) {
+        if (!data.result.success) {
+            fileUploadError(data.result);
+        } else {
+            jQuery('#uploading').slideUp();
+            jQuery('#uploaded').slideDown();
+            jQuery('#filename').val(data.result.files[0].file_id);
+            jQuery('#submitbutton').attr('disabled', '').removeAttr('disabled');
+        }
+    }).bind('fileuploadstart', function() {
+        jQuery('#filechooser').slideUp();
+        jQuery('#uploading').slideDown();
+        jQuery('#fileupload_dropzone').fadeOut();
+    });
+
+    if (jQuery('#fileupload_playback').html() !== 'undefined' && jQuery('#fileupload_playback').html() !== undefined) {
+        if (jQuery.support.cors) {
+            jQuery.ajax({
+                url: 'https://playback.' + UKM_HOSTNAME + '/upload/jQupload_cors.php',
+                type: 'HEAD'
+            }).fail(function() {
+                var result = {
+                    'success': false,
+                    'message': 'Beklager, playbackserveren er ikke tilgjengelig akkurat nå'
+                };
+                fileUploadError(result);
+            });
+        }
+    }
 });
 
 
 jQuery(document).on('click', '.PBdel', function() {
-	return confirm('Er du sikker på at du vil slette denne?');
+    return confirm('Er du sikker på at du vil slette denne?');
 });
 
 ////////////////////////////////////////////////////////////////////////////////////////
