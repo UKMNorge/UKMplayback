2ebfe749fa6bbb7db6476f06a6f1bad21b95f163
Marius Mandal <mariusmandal@gmail.com>
2020-03-22 17:21:15 +0100

    Støtte for fancy nedlasting

--- _playbackserver/zipMePlease/index.php
+++ _playbackserver/zipMePlease/index.php
@@ -2,26 +2,69 @@
 
 use UKMNorge\File\Zip;
 
-if ( $_SERVER['REQUEST_METHOD'] != 'POST' ){
-	error_log('RECIEVED NOT POST DATA. DIE');
-	die( json_encode( false ) );
-}
-$data = json_decode( file_get_contents('php://input') );
-
+require_once('UKM/Autoloader.php');
 require_once('UKMconfig.inc.php');
 
-require_once('UKM/zip.class.php');
+header("Content-type: application/json; charset=utf-8");
+header('Access-Control-Allow-Headers: true');
+header('Access-Control-Allow-Origin: https://' . UKM_HOSTNAME);
+header('Access-Control-Request-Method: OPTIONS, HEAD, GET, POST, PUT, PATCH, DELETE');
+header('Access-Control-Allow-Credentials: true');
 
-// Create zip (and overwrite existing file)
-$zip = new Zip( $data->filename, true );
-$zip->debugMode();
-
-foreach( $data->files as $path => $name ) {
-	$zip->add( dirname( dirname( __FILE__ ) ) . '/'.  $path, $name );
+if ( $_SERVER['REQUEST_METHOD'] != 'POST' ){
+	error_log('RECIEVED NOT POST DATA. DIE');
+	die( json_encode(
+        [
+            'success' => false,
+            'message' => 'Fant ingen filer'
+        ]
+    ));
 }
 
-$url = $zip->compress();
+$data = $_POST['data'];
+
+// Create zip (and overwrite existing file)
+$zip = new Zip( $data['name'], true );
+$zip->tryCatchAdd();
+
+$errors = [];
+
+foreach( $data['files'] as $fil) {
+    #echo $fil['path'] ." => " . $fil['navn'] . "\r\n";
+	try {
+        $res = $zip->add( dirname( dirname( __FILE__ ) ) . '/'.  $fil['path'], $fil['navn'] );
+        if( is_string($res)) {
+            throw new Exception($res);
+        }
+    } catch( Exception $e ) {
+        $errors[] = [
+            'type' => 'fil',
+            'innslag' => $fil['innslag'],
+            'navn' => $fil['playback_navn'],
+            'link' => $fil['playback_link'],
+            'name' => $fil['navn'],
+            'error' => $e->getMessage()
+        ];
+    }
+}
+
+try {
+    $url = $zip->compress();
+} catch( Exception $e ) {
+    $errors[] = [
+        'type' => 'zip',
+        'name' => 'Lagring av zip-fil',
+        'error' => $e->getMessage()
+    ];
+}
 
 $url = str_replace('download.ukm.no/zip', 'playback.ukm.no/zipMePlease/data', $url);
-$result = array('success' => true, 'url' => $url);
+$result = [
+    'success' => true,
+    'url' => $url,
+    'data' => is_string($data),
+    'hasErrors' => sizeof($errors) > 0,
+    'errors' => $errors,
+    'POST' => $_POST
+];
 die( json_encode( $result ) );
