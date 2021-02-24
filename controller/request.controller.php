8e5d61246a5259d8b23121f4ab88ea4cfa6f2383
slogan-15 <kushtrimaliu19@gmail.com>
2021-02-24 16:35:07 +0100

    Send SMS funker

--- controller/request.controller.php
+++ controller/request.controller.php
@@ -2,5 +2,21 @@
 
 use UKMNorge\Arrangement\Arrangement;
 
-// UKMplayback::require('delete/playback.delete.php');
-UKMplayback::addViewData('arrangement', new Arrangement( get_option( 'pl_id' ) ));
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Post to server
+    UKMplayback::addViewData('avsender', $_POST['avsender']);
+    UKMplayback::addViewData('melding', $_POST['melding']);
+
+    // hvis ingen mottaker
+    if(!isset($_POST['innslag'])) {
+        UKMplayback::getFlash()->error('Må ha mottaker');
+    }
+    else {
+        UKMplayback::addViewData('mottakere', $_POST['innslag']);
+        UKMplayback::setAction('sendRequest');
+    }
+}
+else {
+    // UKMplayback::require('delete/playback.delete.php');
+    UKMplayback::addViewData('arrangement', new Arrangement( get_option( 'pl_id' ) ));
+}
