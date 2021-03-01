a47cbb6197ec5c2b08963d3e474f06a270a11a20
slogan-15 <kushtrimaliu19@gmail.com>
2021-03-01 19:49:25 +0100

    Cross-domain endringer

--- _playbackserver/upload/jQupload_cors.php
+++ _playbackserver/upload/jQupload_cors.php
@@ -3,8 +3,21 @@ require_once('UKMconfig.inc.php');
 
 ################################################
 ## SET ALL HEADERS AND ACTUALLY PERFORM UPLOAD 
+
+$origin = $_SERVER['HTTP_ORIGIN'];
+
+$acceptedOrigins = [
+    'https://delta.' . UKM_HOSTNAME, // delta.ukm.no eller delta.ukm.dev
+    'https://' . UKM_HOSTNAME        
+];
+
+$allowOrigin = null;
+if(in_array($origin, $acceptedOrigins)) {
+    $allowOrigin = $origin;
+}
+
 header('Access-Control-Allow-Headers: true');
-header('Access-Control-Allow-Origin: https://' . UKM_HOSTNAME);
+header('Access-Control-Allow-Origin: ' . $allowOrigin);
 header('Access-Control-Request-Method: OPTIONS, HEAD, GET, POST, PUT, PATCH, DELETE');
 header('Access-Control-Allow-Credentials: true');
 
